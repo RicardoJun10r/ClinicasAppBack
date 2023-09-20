d0e3a58a844f007ca183b53c0dcb35a216f26fd4
RicardoJun10r <ricardojunior0110@gmail.com>
2023-09-20 15:14:19 -0300

    att

--- online/src/main/java/com/agendamentos/online/shared/PacienteResponse.java
+++ online/src/main/java/com/agendamentos/online/shared/PacienteResponse.java
@@ -1,18 +1,8 @@
 package com.agendamentos.online.shared;
 
-import java.time.LocalDate;
-import java.util.UUID;
-
 import lombok.Data;
 
 @Data
 public class PacienteResponse {
-    private UUID uuid;
     private String name;
-    private LocalDate createdAt;
-    private String imageURL;
-    private String login;
-    private String password;
-    private Boolean cargo;
-    private String cpf;
 }
